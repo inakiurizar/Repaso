d2cf067582a1c0aee26315c2c160d8b4da314ed1
inakiurizar <101410794+inakiurizar@users.noreply.github.com>
2022-05-17 15:47:51 -0300

    Initial commit

---
+++ Assets/Scripts/BrickCloner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour {
+
+    public GameObject brickPrefab;
+    // public Text txtUserCloneBricks;
+    // int cloneAmount;
+    int counter = 0;
+    float xOffset;
+
+    public void CloneBrick()
+    {
+
+        //if(txtUserCloneBricks.text != " ")
+        //{
+        //    cloneAmount = int.Parse(txtUserCloneBricks.text);
+        //}
+
+        if (counter < 5)
+        {
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            // Destroy(clon, 2);
+            clon.transform.position = new Vector3(xOffset, 0, 0);
+            xOffset += 1.1f;
+            counter++; 
+        }
+            
+        }
+    }
