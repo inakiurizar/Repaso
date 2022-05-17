56aa55b041d661383638a9cccf5e355d018beaa8
inakiurizar <101410794+inakiurizar@users.noreply.github.com>
2022-05-17 15:57:08 -0300

    Repaso

--- Assets/Scripts/BrickCloner.cs
+++ Assets/Scripts/BrickCloner.cs
@@ -6,26 +6,31 @@ using UnityEngine.UI;
 public class BrickCloner : MonoBehaviour {
 
     public GameObject brickPrefab;
-    // public Text txtUserCloneBricks;
+    public Text txtOffset;
     // int cloneAmount;
     int counter = 0;
     float xOffset;
 
+
+    void start()
+    {
+        
+    }
+
     public void CloneBrick()
     {
-
-        //if(txtUserCloneBricks.text != " ")
-        //{
-        //    cloneAmount = int.Parse(txtUserCloneBricks.text);
-        //}
-
+        if(counter == 0)
+        {
+            xOffset = float.Parse(txtOffset.text);
+        }
+         
         if (counter < 5)
         {
+
             GameObject clon;
             clon = Instantiate(brickPrefab);
             // Destroy(clon, 2);
-            clon.transform.position = new Vector3(xOffset, 0, 0);
-            xOffset += 1.1f;
+            clon.transform.position = new Vector3(float.Parse(txtOffset.text), 0, 0);
             counter++; 
         }
             
